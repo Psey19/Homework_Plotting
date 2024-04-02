3bcbc4bcfda1004b9c71ae87238c31cf31f112cb
Psey19 <153425698+Psey19@users.noreply.github.com>
2024-04-02 19:48:52 +0500

    Update HW.py

--- HW.py
+++ HW.py
@@ -10,7 +10,6 @@ lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI': lst})
-data.head()
 data.loc[data['whoAmI'] == 'human', 'human'] = 1
 data.loc[data['whoAmI'] == 'robot', 'robot'] = 1
 data.loc[data['whoAmI'] != 'human', 'human'] = 0
